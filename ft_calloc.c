928fd6d6b83f53d87fca85e20814fb5f35d0355f
trzymamryzwszafce <szymonmadeja523@gmail.com>
2024-12-05 02:04:30 +0100

    kolejne

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: szmadeja <szmadeja@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/05 01:34:47 by szmadeja          #+#    #+#             */
+/*   Updated: 2024/12/05 01:55:34 by szmadeja         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*malloc(size_t nmem, size_t n)
+{
+	void	*ptr;
+
+	ptr = (void *)malloc(nmem * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, nmem);
+	return (ptr);
+}
---
+++ ft_strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: szmadeja <szmadeja@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/05 01:40:21 by szmadeja          #+#    #+#             */
+/*   Updated: 2024/12/05 01:57:09 by szmadeja         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	int			i;
+	const char	*dup;
+
+	i = 0;
+	while (s[i])
+		i++;
+	dup = (char *)malloc(sizeof(char) * (i + 1));
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
