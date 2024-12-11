da06b2944b48ef8932c02c23363686b06b5577dd
trzymamryzwszafce <szymonmadeja523@gmail.com>
2024-12-11 03:05:20 +0100

    d

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,13 +6,30 @@
 /*   By: szmadeja <szmadeja@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 02:07:56 by szmadeja          #+#    #+#             */
-/*   Updated: 2024/12/10 02:17:48 by szmadeja         ###   ########.fr       */
+/*   Updated: 2024/12/11 03:04:52 by szmadeja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd(int nb, int fd)
 {
-
+	if (nb == -2147483648)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putchar_fd('2', fd);
+		nb = 147483648;
+	}
+	if (nb < 0)
+	{
+		ft_putchar_fd('-'. fd);
+		nb *= -1;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar_fd(nb + 48, fd);
 }
